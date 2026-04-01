31ea1ba162bf91e6127e8b462dfb6178b1cdf0ba
ashleynmckays-administrator <ashleynmckays@gmail.com>
2026-03-31 17:43:16 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,8 @@
-java
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Lovely Hearts Rewards Backend!');
+});
+
+app.listen(5000, () => console.log('Server running on port 5000'));
